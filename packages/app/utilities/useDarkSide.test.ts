2bb3a50ace8886e0dfda3e54fcf95207eb6dc6f8
Jarrod Medrano <jmedran@gmail.com>
2022-12-27 12:31:08 -0700

    remove unnecessary before statement

--- packages/app/utilities/useDarkSide.test.ts
+++ packages/app/utilities/useDarkSide.test.ts
@@ -3,18 +3,6 @@ import { renderHook, cleanup, fireEvent, screen, waitFor } from '@testing-librar
 import { useDarkSide } from './useDarkSide';
 
 describe('useDarkSide', () => {
-  beforeAll(() => {
-    window.matchMedia = vi.fn().mockImplementation((query) => ({
-      matches: true,
-      media: '(prefers-color-scheme: dark)',
-      onchange: null,
-      addListener: vi.fn(),
-      removeListener: vi.fn(),
-      addEventListener: vi.fn(),
-      removeEventListener: vi.fn(),
-      dispatchEvent: vi.fn(),
-    }));
-  });
   it('should default to dark theme', () => {
     const { result } = renderHook(() => useDarkSide());
     expect(result.current[0]).toBe('dark');
