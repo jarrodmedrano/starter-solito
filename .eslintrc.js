7ee44551ce9653883bf4672f31478ecfd262e969
Jarrod Medrano <jmedran@gmail.com>
2022-12-28 08:55:52 -0700

    add husky, lint staged etc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,31 @@
 module.exports = {
-  extends: 'next',
+  extends: ['plugin:react/recommended', 'plugin:@next/next/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/forbid-prop-types': 'off',
+    // defaultProps rule to be deprecated on function components
+    // https://github.com/reactjs/rfcs/pull/107
+    'react/require-default-props': [
+      'error',
+      {
+        ignoreFunctionalComponents: true,
+      },
+    ],
+  },
   settings: {
     next: {
       rootDir: 'apps/next/',
     },
   },
   root: true,
-}
+};
